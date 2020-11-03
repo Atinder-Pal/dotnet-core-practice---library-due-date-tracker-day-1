4f2b9cddd4b716bb1544eff34766950322a6edb2
Atinder-Pal <atty.pal46@gmail.com>
2020-11-02 17:19:46 -0700

    Initial Commit;

---
+++ LibraryDueDateDay1/LibraryDueDateDay1/Controllers/BookController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryDueDateDay1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryDueDateDay1.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Create(int id, string title, string author, DateTime publicationDate, DateTime checkedOutDate)
+        {
+            try
+            {
+                CreateBook(id, title, author, publicationDate, checkedOutDate);
+            }
+            catch(Exception e)
+            {
+                ViewBag.addMessage = $"Unable to check out book:{e.Message}";
+            }
+            ViewBag.addMessage = $"You have successfully checked out {title} until {checkedOutDate.AddDays(14)}.";
+            
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            ViewBag.list = Books;
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public static List<Book> Books = new List<Book>();
+
+        public void CreateBook(int id, string title, string author, DateTime publicationDate, DateTime checkedOutDate)
+        {           
+            if (Books.Where(book => book.ID == id).ToList().Count == 0)
+            {
+                var newBook = new Book(id, title, author, publicationDate, checkedOutDate);
+                Books.Add(newBook);
+            }
+            else
+            {
+                throw new Exception("ID already exists. Try adding it witha new ID");
+            }
+            
+        }
+    }
+}
---
+++ LibraryDueDateDay1/LibraryDueDateDay1/Models/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryDueDateDay1.Models
+{
+    public class Book
+    {
+        public int ID { get; }
+        public string Title { get; }
+        public string Author { get; }
+        public DateTime PublicationDate { get; }
+        public DateTime CheckedOutDate { get; }
+
+        public DateTime DueDate { get; set; }
+        public DateTime? ReturnedDate { get; set; }
+       
+
+        public Book(int id, string title, string author, DateTime publicationDate, DateTime checkedOutDate)
+        {
+            ID = id;
+            Title = title;
+            Author = author;
+            PublicationDate = publicationDate;
+            CheckedOutDate = checkedOutDate;
+            DueDate = CheckedOutDate.AddDays(14);
+            ReturnedDate = null;
+        }
+        public Book()
+        {
+            ID = -1;
+            Title = "Defaulttitle";
+            Author = "Default Author";
+            PublicationDate = DateTime.Now;
+            CheckedOutDate = DateTime.Now;
+            DueDate = CheckedOutDate.AddDays(14);
+            ReturnedDate = null;
+        }
+    }
+}
