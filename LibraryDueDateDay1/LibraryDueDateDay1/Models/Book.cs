54a44e7422876b731e2d598467598ac7261d2a2a
Atinder-Pal <atty.pal46@gmail.com>
2020-11-03 14:27:29 -0700

    refactored to introduce backing variables;

--- LibraryDueDateDay1/LibraryDueDateDay1/Models/Book.cs
+++ LibraryDueDateDay1/LibraryDueDateDay1/Models/Book.cs
@@ -9,14 +9,21 @@ namespace LibraryDueDateDay1.Models
     {
         private DateTime _dueDate;
         private DateTime? _returnedDate;
-        public int ID { get; }
-        public string Title { get; }
-        public string Author { get; }
-        public DateTime PublicationDate { get; }
-        public DateTime CheckedOutDate { get; }
+        private int _id;
+        public int ID => _id;
 
-        //public DateTime DueDate { get; set; }
-        //public DateTime? ReturnedDate { get; set; }
+        private string _title;
+        public string Title => _title;
+
+        private string _author;
+        public string Author => _author;
+
+        private DateTime _publicationDate;
+        public DateTime PublicationDate => _publicationDate;
+
+        private DateTime _checkedOutDate;
+        public DateTime CheckedOutDate => _checkedOutDate;
+
         public DateTime DueDate
         {
             get
@@ -42,23 +49,13 @@ namespace LibraryDueDateDay1.Models
 
         public Book(int id, string title, string author, DateTime publicationDate, DateTime checkedOutDate)
         {
-            ID = id;
-            Title = title;
-            Author = author;
-            PublicationDate = publicationDate;
-            CheckedOutDate = checkedOutDate;
+            _id = id;
+            _title = title;
+            _author = author;
+            _publicationDate = publicationDate;
+            _checkedOutDate = checkedOutDate;
             DueDate = CheckedOutDate.AddDays(14);
             ReturnedDate = null;
-        }
-        //public Book()
-        //{
-        //    ID = -1;
-        //    Title = "Defaulttitle";
-        //    Author = "Default Author";
-        //    PublicationDate = DateTime.Now;
-        //    CheckedOutDate = DateTime.Now;
-        //    DueDate = CheckedOutDate.AddDays(14);
-        //    ReturnedDate = null;
-        //}
+        }       
     }
 }
